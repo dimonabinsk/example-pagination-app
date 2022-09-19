611104ee8625b5f834746a9d1f92e7058d494ad1
dimonabinsk <dimonabinsk@yandex.ru>
2022-09-19 11:42:55 +0300

    Первоначальная настройка закончена

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import EpisodesList from "./components/episodesList";
 
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <EpisodesList/>
     </div>
   );
 }
---
+++ src/components/episodesList.jsx
@@ -0,0 +1,17 @@
+import { episodes } from "../fakeStorage/episodes";
+import Episode from "./episode";
+
+const EpisodesList = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        {/* Добавим вывод эпизодов */}
+        {episodes.map((episode) => (
+          <Episode key={episode.id} {...episode} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EpisodesList;
