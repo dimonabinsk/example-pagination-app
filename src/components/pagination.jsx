1ee3131519a22405d85ce7b7695b573936649980
dimonabinsk <dimonabinsk@yandex.ru>
2022-09-19 12:23:55 +0300

    добавил типизацию propTypes

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import PropTypes from 'prop-types';
 
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
@@ -22,5 +23,12 @@ const Pagination = (props) => {
     </nav>
   );
 };
+
+Pagination.propTypes = {
+    itemsCount: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired, 
+    currentPage: PropTypes.number.isRequired,
+}
 
 export default Pagination;
